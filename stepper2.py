5d1257d335d95cd4394e201f976f8dd66ea8240c
star_dan <daaremu1@gmail.com>
2025-06-04 17:16:19 +0100

    feat: response

--- stepper2.py
+++ stepper2.py
@@ -10,6 +10,9 @@ stepper_motor7 = stepper.HalfStepMotor.frompins(IN25, IN26, IN27, IN28, stepms =
 stepper_motor8 = stepper.HalfStepMotor.frompins(IN29, IN30, IN31, IN32, stepms = 2)
 
 
+communicate = Communication()
+communicate.sendMessage("MOVE 500")
+
 def move(step=1000):
     for motor in [stepper_motor5, stepper_motor6, stepper_motor7, stepper_motor8]:
             motor.step(step)
@@ -20,6 +23,8 @@ def await_response():
         status, message = communicate.recieve_message()
         if status == "success" and message[0] == Actions.MOVE:
             move(message[1])
+            communicate.sendMessage("STOP 0")
+            response = False
         elif status == "success" and message[0] == Actions.STOP:
             response = False
         response = False
@@ -28,8 +33,7 @@ def await_response():
 wifi = Wifi()
 wifi.set_AP()
 
-communicate = Communication()
-communicate.sendMessage("MOVE 500")
+
 res = await_response()
 
 
